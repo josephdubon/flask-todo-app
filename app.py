f527908dc61a080d17fc63319befacb64303b3ad
Joseph Dubon <josephdubon@pm.me>
2022-02-14 08:54:14 -0800

    app.py: added db config with sqlite and added class model for TODO

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-# import flask
+from datetime import datetime
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # add app
 app = Flask(__name__)
+# add app config and db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+# class model for functionality
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # a function that will return a string when we create a new element
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 
 # give default route a place to live
